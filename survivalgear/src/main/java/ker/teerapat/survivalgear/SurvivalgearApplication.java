470fb54c824304a24fbda0e304d5712dc997ab50
Teerapat6509650468 <144798820+Teerapat6509650468@users.noreply.github.com>
2025-02-19 19:03:45 +0700

    Add files via upload

---
+++ survivalgear/src/main/java/ker/teerapat/survivalgear/SurvivalgearApplication.java
@@ -0,0 +1,13 @@
+package ker.teerapat.survivalgear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivalgearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurvivalgearApplication.class, args);
+	}
+
+}
